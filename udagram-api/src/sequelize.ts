ea0f14325279ca9e5017cc14664c8fbb83b79ed3
unknown <youssefhany30@yahoo.com>
2022-04-16 23:37:52 +0200

    Using new bucket

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -2,12 +2,12 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port: config.port,
+	username: config.username,
+	password: config.password,
+	database: config.database,
+	host: config.host,
+	port: config.port,
 
-  dialect: "postgres",
-  storage: ":memory:",
+	dialect: "postgres",
+	storage: ":memory:",
 });
